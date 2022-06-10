59064317c025c5f7acbd673302191905baf5d029
chato1337 <chato1337@gmail.com>
2022-06-09 20:25:44 -0500

    implements logic register form

--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -2,18 +2,26 @@ import { SubmitHandler, useForm } from "react-hook-form"
 import { User } from "../models/User"
 import { useMutation } from 'react-query';
 import { Auth } from "../services/Api";
+import { useNavigate } from 'react-router-dom';
+import { toast } from "react-toastify";
 
 export const useRegister = () => {
     const { register, handleSubmit, reset, formState: {errors} } = useForm<User>()
+    const navigate = useNavigate()
+    const notify = (msj: string) => toast(msj, { autoClose: 5000 })
 
     const onSubmit: SubmitHandler<User> = (formData) => {
-        console.log({formData})
         mutate(formData)
     }
 
     const { mutate } = useMutation(Auth.createAccount, {
-        onSuccess: () => {
-            console.log('created')
+        onSuccess: (data, variables, context) => {
+            navigate('/login', { replace: true })
+            notify(`user created!`)
+            reset()
+        },
+        onError: (data, variables, context) => {
+            notify(`[error]: user or email already exist!`)
         }
     })
 
